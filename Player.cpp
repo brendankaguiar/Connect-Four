a528806e282104bb13596e65c9df1ae84bf5ecbe
Brendan Aguiar <50648429+brendankaguiar@users.noreply.github.com>
2022-08-18 19:44:42 -0700

    Update Player.cpp

--- Player.cpp
+++ Player.cpp
@@ -5,8 +5,8 @@
 //  Created by Brendan Aguiar on 8/6/22.
 
 #include "Player.hpp"
-Player::Player(char _token) :token(_token){}
-Player::Player(){};
+Player::Player(char _token) :token(_token) {}
+Player::Player() {};
 char Player::get_token()
 {
     return token;
