828f17aab039950842f7ab43573c26d61f8258d2
Brendan Aguiar <50648429+brendankaguiar@users.noreply.github.com>
2022-08-19 17:20:28 -0700

    Update Board.hpp

--- Board.hpp
+++ Board.hpp
@@ -1,23 +1,38 @@
 //
-//  Player.hpp
+//  Board.hpp
 //  Connect4
 //
 //  Created by Brendan Aguiar on 8/6/22.
 //
 
-#ifndef Player_hpp
-#define Player_hpp
+#ifndef Board_hpp
+#define Board_hpp
 #include <iostream>
+#include <vector>
+#include <string>
+#include "Player.hpp"
 using namespace std;
-
-class Player
+class Board
 {
 private:
-    char token;
+    int grid[6][7];
+    Player player[2];
+    char winner;
 public:
-    Player();
-    Player(char _token);
-    char get_token();
+    Board();
+    void apply_states(int state[][7]);
+    void set_players(const Player p[]);
+    void display_grid() const;
+    void make_move(const Player& p);
+    void make_move_AI(const Player& p, const int& _move);
+    bool check_for_winner(const Player& p) const;
+    void set_winner(const Player& p);
+    char get_winner() const;
+    bool check_diagN(const Player& p) const;
+    bool check_diagP(const Player& p) const;
+    bool check_vertical(const int _grid[][7], const Player& p) const;
+    int get_space(const int& row, const int& col) const;
+    bool check_for_tie() const;
 };
 
-#endif /* Player_hpp */
+#endif /* Board_hpp */
