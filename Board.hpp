9294411838da57762b4caad0921f5d0deecf956e
Brendan Aguiar <50648429+brendankaguiar@users.noreply.github.com>
2022-08-24 18:36:17 -0700

    Update Board.hpp

--- Board.hpp
+++ Board.hpp
@@ -1,38 +1,36 @@
 //
 //  Board.hpp
-//  Connect4
+//  Template Practice
 //
-//  Created by Brendan Aguiar on 8/6/22.
+//  Created by Brendan Aguiar on 8/24/22.
 //
 
 #ifndef Board_hpp
 #define Board_hpp
-#include <iostream>
-#include <vector>
-#include <string>
 #include "Player.hpp"
+#include <iostream>
 using namespace std;
+
+template <typename Datatype>
 class Board
 {
 private:
-    int grid[6][7];
-    Player player[2];
-    char winner;
+    Datatype grid[6][7];
+    Player<Datatype> player[2];
+    Datatype winner;
+    Datatype empty_space;
 public:
-    Board();
-    void apply_states(int state[][7]);
-    void set_players(const Player p[]);
+    Board(Datatype empty_space);
+    void set_players(const Player<Datatype> p[]);
+    void make_move(const Player<Datatype>& p);
+    void make_move_AI(const int& _move);
+    bool check_for_winner(const Player<Datatype>& p) const;
+    void set_winner(const Player<Datatype>& p);
     void display_grid() const;
-    void make_move(const Player& p);
-    void make_move_AI(const Player& p, const int& _move);
-    bool check_for_winner(const Player& p) const;
-    void set_winner(const Player& p);
-    char get_winner() const;
-    bool check_diagN(const Player& p) const;
-    bool check_diagP(const Player& p) const;
-    bool check_vertical(const int _grid[][7], const Player& p) const;
-    int get_space(const int& row, const int& col) const;
+    bool check_diagN(const Player<Datatype>& p) const;
+    bool check_diagP(const Player<Datatype>& p) const;
+    bool check_vertical(const Datatype _grid[][7], const Player<Datatype>& p) const;
     bool check_for_tie() const;
+    Datatype get_winner() const;
 };
-
 #endif /* Board_hpp */
